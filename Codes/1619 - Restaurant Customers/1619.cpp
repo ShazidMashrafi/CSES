3c6929388d0a3bae812a6569fb1c36496c6f7041
shazidmashrafi <90213199+ShazidMashrafi@users.noreply.github.com>
2025-06-25 17:53:59 +0600

    Add solution for 1619 Restaurant Customers

---
+++ Codes/1619 - Restaurant Customers/1619.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+#ifdef ONLINE_JUDGE
+#define dbg(...)
+#else
+#include <debug.h>
+#endif
+#define  ll  long long
+#define  endl  '\n'
+#define  ff  first
+#define  ss  second
+#define  pb  push_back
+#define  sz(x)  (int)(x).size()
+#define  all(x)  x.begin(), x.end()
+#define  Dpos(n) fixed << setprecision(n)
+#define  yn(f)  f? cout<<"YES\n":cout<<"NO\n"
+#define  FAST  (ios_base::sync_with_stdio(false), cin.tie(nullptr));
+ll power(ll x,ll y,ll m=LLONG_MAX) {ll ans=1;x%=m;while(y){if(y&1)ans=(ans*x)%m;x=(x*x)%m;y>>=1;}return ans;}
+
+void solve()
+{
+    int n;
+    cin >> n;
+    map<ll, int> mp;
+    for (int i = 0; i < n; ++i) 
+    {
+        ll a, b;
+        cin >> a >> b;
+        mp[a] += 1;
+        mp[b] -= 1;
+    }
+    int ct = 0, mx = 0;
+    for (auto &e : mp) {
+        ct += e.second;
+        mx = max(mx, ct);
+    }
+    cout << mx << endl;
+}
+
+signed main()
+{
+    FAST;
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+
+    int TCS = 1;
+    // cin >> TCS;
+    for (int TC = 1; TC <= TCS; ++TC)
+    {
+        // cout<<"Case "<<TC<<": ";
+        solve();
+    }
+}
