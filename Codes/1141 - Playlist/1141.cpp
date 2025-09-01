9cb5016b271b35a3c2f70a0d4a3ad8878d9ef052
shazidmashrafi <90213199+ShazidMashrafi@users.noreply.github.com>
2025-09-02 00:45:59 +0600

    Add solution for 1234 New Problem

---
+++ Codes/1141 - Playlist/1141.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+#ifdef ONLINE_JUDGE
+#define dbg(...)
+#else
+#include <debug.h>
+#endif
+#define  ll  long long
+#define  endl  '\n'
+#define  ff  first
+#define  ss  second
+#define  pb  push_back
+#define  sz(x)  (int)(x).size()
+#define  all(x)  x.begin(), x.end()
+#define  Dpos(n) fixed << setprecision(n)
+#define  yn(f)  f? cout<<"YES\n":cout<<"NO\n"
+#define  FAST  (ios_base::sync_with_stdio(false), cin.tie(nullptr));
+ll power(ll x,ll y,ll m=LLONG_MAX) {ll ans=1;x%=m;while(y){if(y&1)ans=(ans*x)%m;x=(x*x)%m;y>>=1;}return ans;}
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int>songs(n);
+    for(int i = 0; i < n; ++i) {
+        cin >> songs[i];
+    }
+    map<int, int> pos;
+    int l = 0, len = 0;
+    for(int r = 0; r < n; ++r) {
+        int song  = songs[r];
+        if(pos.find(song) != pos.end() && pos[song] >= l) {
+            l = pos[song] + 1;
+        }
+
+        pos[song] = r;
+        len = max(len, r - l + 1);
+    }
+    cout << len << endl;
+}
+
+signed main()
+{
+    FAST;
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+
+    int TCS = 1;
+    // cin >> TCS;
+    for (int TC = 1; TC <= TCS; ++TC)
+    {
+        // cout<<"Case "<<TC<<": ";
+        solve();
+    }
+}
